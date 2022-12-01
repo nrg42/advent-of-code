10b3dae827fd785334d9d2ed1002bf97ec31d2b6
Noe <nrg904@gmail.com>
2022-12-01 13:33:10 -0500

    complete day 1 puzzles

---
+++ 2022/day-1.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const input = fs.readFileSync('./day-1-input.txt', 'utf-8').trim().split('\n\n')
+
+const part1 = (input) => {
+    let max = 0
+
+    for (let elf of input) {
+        let curElfTotal = elf.split('\n').reduce((total,val) => total + parseInt(val), 0)
+        if (curElfTotal > max) max = curElfTotal   
+    }
+
+    return max
+}
+
+
+const part2 = (input) => {
+    const maxList = [0,0,0]
+    
+    for (let elf of input) {
+        let currElfTotal = elf.split('\n').reduce((total,val) => total + parseInt(val), 0)
+        if (currElfTotal > maxList[0]) {
+            maxList[0] = currElfTotal
+            maxList.sort((a,b) => a - b)
+        }
+    }
+
+    return maxList.reduce((total, curVal) => total + curVal)
+}
+
+console.log(`part 1 answer: ${part1(input)}`)
+console.log(`part 2 answer: ${part2(input)}`)
